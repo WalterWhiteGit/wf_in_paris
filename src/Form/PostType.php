4ee5a0967a4f088f9dc70a423de359b7fbc83119
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-02-07 21:32:00 +0100

    Add

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Post;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,8 +16,31 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+
+        // Champ Titre du post.
             ->add('title',TextType::class)
+
+        // Champ Description du post.
             ->add('content',TextareaType::class)
+
+        // Champ Image du post.
+            ->add('image',FileType::class)
+
+        //Champ Auteur du post.
+            ->add('author',EntityType::class,
+                ['class'=>'App\Entity\Author',
+                  'choice_label'=>'Firstname',
+                  'multiple'=>false
+                ]
+                )
+        //Champ catégorie du post.
+            ->add('category',EntityType::class,
+                 ['class'=>'App\Entity\Category',
+                  'choice_label'=>'name',
+                  'multiple'=>false
+                     ]
+                 )
+
             ;
 
     }
